6711f613806314a3c05ead751c021f11b4ff8a2a
RaphelFrey <info@raphaelfrey.com>
2023-04-07 17:28:04 +0200

    Added AuraCharacter and AuraEnemy

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Raphael Frey
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Raphael Frey
+
+
+#include "Character/AuraEnemy.h"
+
